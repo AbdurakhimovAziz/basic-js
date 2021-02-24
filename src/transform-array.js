ec5ba95ed077a7332326a5fabbedfdfba91f9af2
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-02-24 20:15:26 +0500

    feat: finish what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let transformedArr = arr ? arr.slice() : [];
+  transformedArr.forEach((element, index) => {
+    if (element === '--discard-next') {
+      transformedArr.splice(index, 2);
+    }
+    if (element === '--discard-prev') {
+      index === 0 ? transformedArr.splice(index, 1) : transformedArr.splice(index - 1, 2);
+    }
+    if (element === '--double-next') {
+      index === transformedArr.length - 1 ? transformedArr.splice(index, 1) : transformedArr.splice(index, 1, transformedArr[index + 1]);
+    }
+    if (element === '--double-prev') {
+      index === 0 ? transformedArr.splice(index, 1) : transformedArr.splice(index, 1, transformedArr[index - 1]);
+    }
+  });
+  return transformedArr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -3,6 +3,7 @@ const seasons = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'su
 
 module.exports = function getSeason(date) {
   if (date === undefined) return 'Unable to determine the time of year!';
+  if (Object.keys(date).length > 0 || typeof date !== 'object') throw 'Error';
   const month = date.getMonth();
   return seasons[month];
   // remove line with error and write your code here
