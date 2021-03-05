2173f1f7eaa1b48473d7a598afba3f71a20bab17
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-03-05 23:10:59 +0500

    feat: finish extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,19 +1,19 @@
 const CustomError = require('../extensions/custom-error');
 
-module.exports = function repeater(str, options) {
-  let result = '';
-  if (options.separator == undefined) options.separator = '+';
-  if (options.additionSeparator == undefined) options.additionSeparator = '||';
-  if (options.repeatTimes == undefined) return (result = str + options.addition);
-  for (let i = 0; i < options.repeatTimes; i++) {
-    let additionSeparator = options.additionSeparator;
-    let additionString = '';
-    if (i == options.repeatTimes - 1) options.separator = '';
-    for (let j = 0; j < options.additionRepeatTimes; j++) {
-      if (j == options.additionRepeatTimes - 1) additionSeparator = '';
-      additionString = additionString + options.addition + additionSeparator;
+module.exports = function repeater(str, obj) {
+  let repeatTimes = obj.repeatTimes == undefined ? 1 : obj.repeatTimes;
+  let separator = obj.separator + '' == 'undefined' ? '+' : obj.separator;
+  let addition = obj.addition + '' == 'undefined' ? '' : obj.addition;
+  let additionRepeatTimes = obj.additionRepeatTimes == undefined ? 1 : obj.additionRepeatTimes;
+  let additionSeparator = obj.additionSeparator == undefined ? '|' : obj.additionSeparator;
+  let answer = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    answer += str;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      answer += addition;
+      j == additionRepeatTimes - 1 ? answer : (answer += additionSeparator);
     }
-    result = result + str + additionString + options.separator;
+    i == repeatTimes - 1 ? answer : (answer += separator);
   }
-  return result;
+  return answer;
 };
